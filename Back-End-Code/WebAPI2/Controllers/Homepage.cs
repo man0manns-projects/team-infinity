da40727e40965be83aaa460f31edacce8ed9ecef
man0mann <elizabethmanemann@gmail.com>
2022-10-05 23:17:54 -0500

    Current user retrievel and fixing my dumb syntax mistakes

--- Back-End-Code/WebAPI2/Controllers/Homepage.cs
+++ Back-End-Code/WebAPI2/Controllers/Homepage.cs
@@ -19,23 +19,23 @@ namespace WebAPI2.Controllers
         }
 
         //data returned for fundraiser dashboard
-
         [HttpGet]
 
         public JsonResult Get(int current_userid)
         {
             string query = @"
-                    SELECT TOP 8 FROM dbo.fundraiser WHERE user_id != @current_userid";
+                    SELECT TOP 8 * FROM dbo.fundraisers WHERE user_id != @current_userid";
             DataTable table = new DataTable();
             string sqlDataSource = _configuration.GetConnectionString("FundraiserAppCon");
             SqlDataReader myReader;
-            using(SqlConnection myCon = new SqlConnection (sqlDataSource))
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
             {
                 myCon.Open();
                 using (SqlCommand myCommand = new SqlCommand(query, myCon))
                 {
-                    SqlParameter[] param = new SqlParameter[2];
-                    param[0] = new SqlParameter("@username", current_userid);
+                    SqlParameter[] param = new SqlParameter[1];
+                    param[0] = new SqlParameter("@current_userid", current_userid);
+                    myCommand.Parameters.Add(param[0]);
                     myReader = myCommand.ExecuteReader();
                     table.Load(myReader);
                     myReader.Close();
@@ -46,45 +46,76 @@ namespace WebAPI2.Controllers
             return new JsonResult(table);
         }
 
-    }
 
-    //insert new fundraiser
-    [HttpPost]
-    public JsonResult Post(int user, string fundraiserName, string fundraiserDescription, int initAmount)
-    {
-        //TODO: Add file upload
-        //var imageDataByteArray = Convert.FromBase64String(model.ImageData);
-        //var imageDataStream = new MemoryStream(imageDataByteArray);
-	    //imageDataStream.Position = 0;
+        //get fundraisers for current user
+        [Route("User")]
+        [HttpGet]
 
-        string query = @"
-                    INSERT INTO dbo.fundraiser (user_id, title, txt_description, amount_raised, img_url) VALUES (@userID, @title, @description, @amount, NULL)";
-        DataTable table = new DataTable();
-        string sqlDataSource = _configuration.GetConnectionString("FundraiserAppCon");
-        SqlDataReader myReader;
-        using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+        public JsonResult GetMyFundraisers(int my_userid)
         {
-            myCon.Open();
-            using (SqlCommand myCommand = new SqlCommand(query, myCon))
+            string query = @"
+                    SELECT TOP 8 * FROM dbo.fundraisers WHERE user_id = @current_userid";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("FundraiserAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
             {
-                SqlParameter[] param = new SqlParameter[4];
-                param[0] = new SqlParameter("@userID", user);
-                param[1] = new SqlParameter("@title", fundraiserName);
-                param[2] = new SqlParameter("@description", fundraiserDescription);
-                param[3] = new SqlParameter("@amount", initAmount);
-                myCommand.Parameters.Add(param[0]);
-                myCommand.Parameters.Add(param[1]);
-                myCommand.Parameters.Add(param[2]);
-                myCommand.Parameters.Add(param[3]);
-                //TODO: Integrate with ReactJS variables
-                myReader = myCommand.ExecuteReader();
-                table.Load(myReader);
-                myReader.Close();
-                myCon.Close();
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    SqlParameter[] param = new SqlParameter[1];
+                    param[0] = new SqlParameter("@current_userid", my_userid);
+                    myCommand.Parameters.Add(param[0]);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
             }
+
+            return new JsonResult(table);
         }
 
-        return new JsonResult("200");
+        //insert new fundraiser
+        [HttpPost]
+        public JsonResult Post(int user, string fundraiserName, string fundraiserDescription, int initAmount)
+        {
+            //TODO: Add file upload
+            //var imageDataByteArray = Convert.FromBase64String(model.ImageData);
+            //var imageDataStream = new MemoryStream(imageDataByteArray);
+            //imageDataStream.Position = 0;
+
+            string query = @"
+                    INSERT INTO dbo.fundraisers (user_id, title, txt_description, amount_raised, img_url) VALUES (@userID, @title, @description, @amount, NULL)";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("FundraiserAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    SqlParameter[] param = new SqlParameter[4];
+                    param[0] = new SqlParameter("@userID", user);
+                    param[1] = new SqlParameter("@title", fundraiserName);
+                    param[2] = new SqlParameter("@description", fundraiserDescription);
+                    param[3] = new SqlParameter("@amount", initAmount);
+                    myCommand.Parameters.Add(param[0]);
+                    myCommand.Parameters.Add(param[1]);
+                    myCommand.Parameters.Add(param[2]);
+                    myCommand.Parameters.Add(param[3]);
+                    //TODO: Integrate with ReactJS variables
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("200");
+
+        }
 
     }
 }
+
