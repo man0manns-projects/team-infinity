9ffa27edae156341a22ec4f1c3a882b60932a77d
man0mann <elizabethmanemann@gmail.com>
2022-10-14 16:09:14 -0500

    adding the listening URL to be the SQL server

---
+++ Back-End-Code/WebAPI2/Controllers/FundraiserPage.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using WebAPI2.Models;
+
+namespace WebAPI2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FundraiserController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public FundraiserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+       
+    }
+
+
+}
+
