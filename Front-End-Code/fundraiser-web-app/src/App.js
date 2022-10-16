8cb62c0d54720baf92fa36866cf76fde0868110c
man0mann <elizabethmanemann@gmail.com>
2022-10-15 20:25:27 -0500

    adding basic ReactJS app structure

---
+++ Front-End-Code/fundraiser-web-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+     <h3 className="m-3 d-flex justify-content-center">
+       React JS Tutorial
+     </h3>
+     </div>
+  );
+}
+
+export default App;
