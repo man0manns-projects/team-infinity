fb2447329805814cc7111406872c9f5588929258
melngu11 <melnguyen11@gmail.com>
2022-10-17 16:52:02 -0500

    Login page

--- Front-End-Code/fundraiser-web-app/src/App.js
+++ Front-End-Code/fundraiser-web-app/src/App.js
@@ -1,16 +1,46 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom';
+import {Navbar,Nav} from 'react-bootstrap';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 import {Home} from './Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import { Login } from './Login';
 function App() {
   return (
-    <BrowserRouter>
-          <Routes>
-              <Route exact path='/' element={<Home />} />
-          </Routes>
-  </BrowserRouter>
-  );
-}
 
-export default App;
+    <BrowserRouter>
+              <Navbar bg="dark" expand="lg">
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav>
+                <NavLink className="d-inline p-2 bg-dark text-white" to="/">
+                    Home
+                </NavLink>
+                <NavLink className="d-inline p-2 bg-dark text-white" to="/Fundraisers">
+                    Fundraisers
+                </NavLink>
+                <NavLink className="d-inline p-2 bg-dark text-white" to="/Login">
+                    Login
+                </NavLink>
+
+                </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+      <div className="App">
+ 
+        <div className="auth-wrapper">
+          <div className="auth-inner">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/sign-in" element={<Login />} />
+
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+  )
+}
+export default App
