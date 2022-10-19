accf591a4a569fb8b7ecb65be141bd074fe24016
man0mann <elizabethmanemann@gmail.com>
2022-10-18 22:10:37 -0500

    adding the fundraiser page testing

--- Front-End-Code/fundraiser-web-app/src/App.js
+++ Front-End-Code/fundraiser-web-app/src/App.js
@@ -1,13 +1,15 @@
 
 
 import {Home} from './Home';
+import { ViewFundraiserModal } from './ViewFundraiserModal';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
           <Routes>
-              <Route exact path='/' element={<Home />} />
+              <Route path='/' element={<Home />} />
+              <Route exact path='/test-fundraiser' element={<ViewFundraiserModal />} />
           </Routes>
   </BrowserRouter>
   );
---
+++ Front-End-Code/fundraiser-web-app/src/ViewFundraiserModal.js
@@ -0,0 +1,102 @@
+import React,{Component} from 'react';
+import {Table} from 'react-bootstrap';
+import {Button,ButtonToolbar} from 'react-bootstrap';
+import ProgressBar from 'react-bootstrap/ProgressBar';
+
+import './App.css';
+
+export class ViewFundraiserModal extends Component{
+    
+   constructor(props){
+        super(props);
+        this.state={donors:[],info:[]}
+    }
+
+    //define API methods as functions 
+    refreshDonors(filter){
+        fetch('http://20.169.81.116:5199/api/Fundraiser/Donors?fundraiserID='+ filter)
+        .then(response=>response.json())
+        .then(data=>{
+            this.setState({donors:data})
+        });
+    }
+
+    getFundraiserInfo(filter){
+        fetch('http://20.169.81.116:5199/api/Fundraiser?fundraiserID='+ filter)
+        .then(response=>response.json())
+        .then(data=>{
+            this.setState({info:data})
+        });
+    }
+
+    componentDidMount(){
+        //this.refreshList(this.props.fundId);
+        this.refreshDonors("500");
+        this.getFundraiserInfo("500");
+    }
+    componentDidUpdate(){
+        //this.refreshList(this.props.fundId);
+        this.refreshDonors("500");
+        this.getFundraiserInfo("500");
+    }
+
+    currencyFormat(moneys) {
+        let num = Number(moneys,10);
+        return '$' + num.toFixed(2).replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+    };
+
+    render(){
+        const {donors}=this.state;
+        const {info}=this.state;
+        return(
+        <div >
+            {info.map(info=>
+            <div>
+            <h2 className="mt-5 d-flex justify-content-center">Fundraiser Title: {info.title}</h2>
+            <p className="mt-5 d-flex justify-content-center">Description: {info.txt_description}</p>
+            <ProgressBar max={info.goal}>
+            <ProgressBar variant="success" now={info.amount_raised} label={this.currencyFormat(info.amount_raised)} key={1}/>
+            <ProgressBar variant="NOT_THERE" now={info.goal} label={this.currencyFormat(info.goal)} key={2}/>
+            </ProgressBar>
+            <p className="mt-5 d-flex justify-content-right">{this.currencyFormat(info.goal)}</p>
+            </div>
+            )}
+                <Table className="mt-4" striped bordered hover size="sm">
+                    <thead>
+                        <tr>
+                        <th>Name</th>
+                        <th>Donation_Amount</th>
+                        <th>Notes</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {donors.map(donors=>
+                        <tr>
+                                <td>{donors.f_name}</td>
+                                <td>{this.currencyFormat(donors.donation_amt)}</td>
+                                <td>{donors.notes}</td>
+                                <td>
+                                </td>
+                        </tr>)}
+                    </tbody>
+
+                </Table>
+
+                <ButtonToolbar>
+                    <Button variant='primary'>
+                    Donate</Button>
+                </ButtonToolbar>
+            </div>
+        )
+    }
+
+   /*  render(){
+        return(
+            <div className="mt-5 d-flex justify-content-left">
+                This is Fundraiser page.
+            </div>
+        )
+    } */
+
+}
+
