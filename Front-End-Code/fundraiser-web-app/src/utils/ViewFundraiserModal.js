6d36c4c690ad2cdb232956e4cb83ffb144e9e360
Melisa Nguyen <melnguyen11@gmail.com>
2022-10-25 01:10:05 -0500

    Syntax for folder reorganization

--- Front-End-Code/fundraiser-web-app/src/utils/ViewFundraiserModal.js
+++ Front-End-Code/fundraiser-web-app/src/utils/ViewFundraiserModal.js
@@ -7,7 +7,7 @@ import {Link, BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 
 
 
-import './fund.css';
+import '../css/fund.css';
 
 export class ViewFundraiserModal extends Component{
     
@@ -49,61 +49,61 @@ export class ViewFundraiserModal extends Component{
     render(){
         const {donors}=this.state;
         const {info}=this.state;
-        const logo = require('./images/fund.jpg');
+        const logo = require('../images/fund.jpg');
         return(
-            <main>
-            <div>
- 
-            {info.map(info=>
-            
-            <div className='left'>
-            <h2 className="mt-5 d-flex justify-content-center">{info.title}</h2>
-            <h3 className="mt-5 d-flex justify-content-center">Raised: {this.currencyFormat(info.amount_raised)}     Goal: {this.currencyFormat(info.goal)}</h3>
-        
-            <img src={logo} />
-            
-            
-            <ProgressBar max={info.goal}>
-            <ProgressBar variant="success" now={info.amount_raised} label={this.currencyFormat(info.amount_raised)} key={1}/>
-            <ProgressBar variant="NOT_THERE" now={info.goal} label={this.currencyFormat(info.goal)} key={2}/>
-            </ProgressBar>
-            <p className="mt-4 d-flex justify-content-left">Description: {info.txt_description}</p>
-            
-      
-      </div>
-
-)}
-            
-            <div className='right'>
+                <main>
                 <div>
-                <Table className="table-responsive-sm">
-                <thead>
-                        <tr>
-                        <th>Donor</th>
-                        <th>Donation</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {donors.map(donors=>
-                        <tr>
-                                <td>{donors.f_name}</td>
-                                <td>{this.currencyFormat(donors.donation_amt)}</td>
-  
-                                <td>
-                                </td>
-                        </tr>)}
-                    </tbody>
-
-                </Table>
-                {info.map(info => 
-                <Link to="/TEST-donation-form" state={{title: info.title, id: info.fundraiser_id}} state2={{id: info.fundraiser_id}}>
-                <ButtonToolbar>
-                    <Button variant="btn btn-success btn-lg btn-block">
-                    Donate</Button>
-                </ButtonToolbar>
-                </Link>
-                )}
+     
+                {info.map(info=>
+                
+                <div className='left'>
+                <h2 className="mt-5 d-flex justify-content-center">{info.title}</h2>
+                <h3 className="mt-5 d-flex justify-content-center">Raised: {this.currencyFormat(info.amount_raised)}     Goal: {this.currencyFormat(info.goal)}</h3>
+            
+                <img src={logo} />
+                
+                
+                <ProgressBar max={info.goal}>
+                <ProgressBar variant="success" now={info.amount_raised} label={this.currencyFormat(info.amount_raised)} key={1}/>
+                <ProgressBar variant="NOT_THERE" now={info.goal} label={this.currencyFormat(info.goal)} key={2}/>
+                </ProgressBar>
+                <p className="mt-4 d-flex justify-content-left">Description: {info.txt_description}</p>
+                
+          
+          </div>
+    
+    )}
+                
+                <div className='right'>
+                    <div>
+                    <Table className="table-responsive-sm">
+                    <thead>
+                            <tr>
+                            <th>Donor</th>
+                            <th>Donation</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {donors.map(donors=>
+                            <tr>
+                                    <td>{donors.f_name}</td>
+                                    <td>{this.currencyFormat(donors.donation_amt)}</td>
+      
+                                    <td>
+                                    </td>
+                            </tr>)}
+                        </tbody>
+    
+                    </Table>
+                    </div>
+                    <ButtonToolbar>
+                        <Button variant="btn btn-success btn-lg btn-block">
+                        Donate</Button>
+                    </ButtonToolbar>
+                    </div>
+       
             </div>
-        )
+            </main>
+            )
+        }
     }
-}
