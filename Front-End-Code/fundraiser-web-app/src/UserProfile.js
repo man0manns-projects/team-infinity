f579f0bda1cc1ebe6e2ab0a115f42961bbfd094a
man0mann <elizabethmanemann@gmail.com>
2022-12-03 22:18:31 -0600

    fixing user profile page

--- Front-End-Code/fundraiser-web-app/src/UserProfile.js
+++ Front-End-Code/fundraiser-web-app/src/UserProfile.js
@@ -1,4 +1,4 @@
-import React, {useId} from 'react';
+import React, {useRef, useEffect} from 'react';
 import {Form,Button, Col, Row, InputGroup, Accordion} from 'react-bootstrap';
 import { useState } from 'react';
 import { UsaStates } from 'usa-states';
@@ -10,41 +10,50 @@ import { UsaStates } from 'usa-states';
 // }
 
 export default function UserProfile(){
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState();
+  const userID = sessionStorage.getItem('token');
 
-      const userID = sessionStorage.getItem('token');
-      var usStates = new UsaStates();
-      var statesNames = usStates.arrayOf('names');
+/*         const handleChange = (event) => {
+          let value = event.target.value;
+          let name = event.target.name;
+          this.setState({[name]: value})
+        } */
 
-      // const [currentFundraiser, setFundID] = useState();
-      const [userEmail, setEmail] = useState();
-      const [firstName, setFname] = useState();
-      const [lastName, setLname] = useState();
-      const [password, setPassword] = useState();
-      const [currentAddress, setAddress] = useState();
-      const [currentCity, setCity] = useState();
-      const [currentState, setState] = useState();
-      const [currentZip, setZip] = useState();
+        useEffect(() => {
+          const getUserInfo = async () => {
+            const response = await fetch(process.env.REACT_APP_API+'user?userID='+ userID);
+            const json = await response.json();
+            setFirstName(JSON.parse(JSON.stringify(json))[0].f_name);
+            setLastName(JSON.parse(JSON.stringify(json))[0].l_name);
+            setEmail(JSON.parse(JSON.stringify(json))[0].email_address);
+            setPassword(JSON.parse(JSON.stringify(json))[0].password);
+          };
+          getUserInfo();
+        },[]);
 
-
-
-      let handleSubmit = async (e) => {
-        e.preventDefault();
-        try{
-
-          let res = await fetch('http://20.169.81.116:5199/api/user?userID='+ userID + '&emailAddress=' + userEmail + '&firstName=' + firstName + '&lastName=' + lastName + '&password=' + password + '&streetAddress=' + currentAddress + '&city=' + currentCity + '&zipcode=' + currentZip + '&country=' + currentState , {
-            method:"GET",
-          });
+        let handleSubmit = async (e) => {
+          e.preventDefault();
+          try{
   
-          if(res.status == 200){
-          alert("Submitted successfully");
-          window.location='/';
-        } else{
-          alert("Some error occurred with form submission")
+            let res = await fetch('http://20.169.81.116:5199/api/user?userID='+ userID + '&userEmail=' + email + '&firstName=' + firstName + '&lastName=' + lastName + '&password=' + password , {
+              method:"POST",
+            });
+    
+            if(res.status == 200){
+            alert("Submitted successfully");
+            window.location='/';
+          } else{
+            alert("Some error occurred with form submission")
+          }
+        } catch (err) {
+          console.log(err);
         }
-      } catch (err) {
-        console.log(err);
-      }
-    };
+      };
+
+      console.log(firstName);
 
         return (
           <div className='p-5 dono'>
@@ -53,12 +62,12 @@ export default function UserProfile(){
             <Row className="mb-3">
               <Form.Group as={Col} controlId="donorFirstName">
                 <Form.Label>First Name</Form.Label>
-                <Form.Control type="fname" placeholder="Enter first name" value={firstName} onChange={(e) => setFname(e.target.value)}/>
+                <Form.Control type="fname" placeholder={firstName}  onChange={(e) => setFirstName(e.target.value)}/>
               </Form.Group>
       
-              <Form.Group as={Col} controlId="donorLastName">
+             <Form.Group as={Col} controlId="donorLastName">
                 <Form.Label>Last Name</Form.Label>
-                <Form.Control type="lname" placeholder="Enter last name" value={lastName} onChange={(e) => setLname(e.target.value)}/>
+                <Form.Control type="lname" placeholder={lastName} onChange={(e) => setLastName(e.target.value)}/>
               </Form.Group>
             </Row>
       
@@ -66,57 +75,30 @@ export default function UserProfile(){
             <Row classname="mb-3">
               <Form.Group as={Col} controlId="formGridEmail">
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="address@website.com" value={userEmail} onChange={(e) => setEmail(e.target.value)}/>
+                <Form.Control type="email" placeholder={email} onChange={(e) => setEmail(e.target.value)}/>
               </Form.Group>
             </Row>
-
-            <Form.Group className="mb-3" controlId="formGridAddress1">
-              <Form.Label>Address</Form.Label>
-              <Form.Control placeholder="1234 Main St" value={currentAddress} onChange={(e) => setAddress(e.target.value)}/>
-            </Form.Group>
-      
-            <Row className="mb-3">
-              <Form.Group as={Col} controlId="formGridCity">
-                <Form.Label>City</Form.Label>
-                <Form.Control value={currentCity} onChange={(e) => setCity(e.target.value)}/>
-              </Form.Group>
-      
-              <Form.Group as={Col} controlId="formGridState">
-                <Form.Label>State</Form.Label>
-                <Form.Select defaultValue="Choose..." value={currentState} onChange={(e) => setState(e.target.value)}>
-                  <option>Choose...</option>
-                  {statesNames.map((option, index) => (
-                    <option key={index} value={option}>{option}</option>
-                  ))}
-                </Form.Select>
-              </Form.Group>
-      
-              <Form.Group as={Col} controlId="formGridZip">
-                <Form.Label>Zip</Form.Label>
-                <Form.Control value={currentZip} onChange={(e) => setZip(e.target.value)}/>
-              </Form.Group>
-            </Row>
-
-            <br></br>
             <br></br>
             <Col classname="mb-3">
               <Form.Group as={Col} controlId="formGridPassword">
                 <Form.Label>Old Password</Form.Label>
-                <Form.Control type="password" placeholder="Enter Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <Form.Control type="password"  onChange={(e) => setPassword(e.target.value)}/>
               </Form.Group>
             </Col>
+            <br></br>
             <Col classname="mb-3">
               <Form.Group as={Col} controlId="formGridPassword">
                 <Form.Label>New Password</Form.Label>
-                <Form.Control type="password" placeholder="Enter Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <Form.Control type="password" onChange={(e) => setPassword(e.target.value)}/>
               </Form.Group>
             </Col>
+            <br></br>
             <Col classname="mb-3">
               <Form.Group as={Col} controlId="formGridPassword">
                 <Form.Label>Confirm Password</Form.Label>
-                <Form.Control type="password" placeholder="Enter Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                <Form.Control type="password" onChange={(e) => setPassword(e.target.value)}/>
               </Form.Group>
-            </Col>
+            </Col> 
 
             <br></br>
             <Row classname="mb-3">
